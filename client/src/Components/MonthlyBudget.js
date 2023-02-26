4da6fab818cd47bb9cf3f17770375913aadf37bd
Unaiza898 <unaiza.nizami@cix.csi.cuny.edu>
2023-02-26 18:53:49 -0500

    update

--- client/src/Components/MonthlyBudget.js
+++ client/src/Components/MonthlyBudget.js
@@ -8,9 +8,8 @@ function MonthlyBudget() {
         <h4>Manage your Budget</h4>
         <p>
           {" "}
-          There are many variations of passages of Lorem Ipsum available, but
-          the majority have suffered alteration in some form, by injected
-          humour, or randomised words which don't look even slightly believable{" "}
+          Managing your personal finances can be challenging, but it's an essential part of achieving financial stability and independence. Are you tired of living paycheck to paycheck, struggling with debt, or feeling like you can't make ends meet? If so, it's time to take control of your finances and start managing your money wisely, with quick and clear explanations to everyone!
+By creating a budget, tracking your expenses, and setting financial goals, you can gain a better understanding of your finances and make informed decisions about your money. Whether you're saving for a down payment on a home, paying off student loans, or planning for retirement, managing your personal finances is the first step toward achieving your financial dreams.{" "}
         </p>
       </div>
 
